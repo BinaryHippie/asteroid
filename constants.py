60ab7ef41952bf5e66bcc203abba9dc485ace244
BinaryHippie <binary_hippie@proton.me>
2025-08-12 11:05:10 +0200

    Added turn speed for player

--- constants.py
+++ constants.py
@@ -1,9 +1,14 @@
 # ~/asteroids/constants.py
 
+# Player
 PLAYER_RADIUS = 20
+PLAYER_TURN_SPEED = 300
+
+# Screen
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# Asteroid
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds

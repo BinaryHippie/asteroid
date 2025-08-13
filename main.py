004d6201b88282d8ab4c454e16a35a5d768ff000
BinaryHippie <binary_hippie@proton.me>
2025-08-13 19:21:28 +0200

    Updated main for asteroid and asteroidfield

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)

f718d40882fd2b82d42c4f9a9894cadd457e2897
BinaryHippie <binary_hippie@proton.me>
2025-08-13 19:52:07 +0200

    Added keyboard input to shoot and its method

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 # ~/asteroids/player.py
 
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import * 
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
     
@@ -38,6 +39,14 @@ class Player(CircleShape):
 
         if keys[pygame.K_s]:
             self.move(-dt)
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * SHOT_SPEED
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
